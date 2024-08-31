84acc4188a72b71106c41745e6a035c5af214010
Jordan Bradford <36420801+jrdnbradford@users.noreply.github.com>
2024-08-31 16:26:56 -0400

    Separate mirror functions

--- tests/testthat/test-gutenberg_download.R
+++ tests/testthat/test-gutenberg_download.R
@@ -1,35 +1,3 @@
-test_that("gutenberg_get_mirror works with no option set", {
-  local_dl_and_read()
-  withr::local_options(gutenberg_mirror = NULL)
-  expect_message(
-    expect_identical(
-      gutenberg_get_mirror(),
-      "http://aleph.gutenberg.org"
-    ),
-    "Determining mirror"
-  )
-  expect_no_message(
-    expect_identical(
-      gutenberg_get_mirror(),
-      "http://aleph.gutenberg.org"
-    )
-  )
-})
-
-test_that("gutenberg_get_mirror respects verbose", {
-  local_dl_and_read()
-  withr::local_options(gutenberg_mirror = NULL)
-  expect_no_message(gutenberg_get_mirror(verbose = FALSE))
-})
-
-test_that("gutenberg_get_mirror uses existing option", {
-  local_dl_and_read()
-  withr::local_options(gutenberg_mirror = "mirror")
-  expect_identical(
-    gutenberg_get_mirror(), "mirror"
-  )
-})
-
 test_that("gutenberg_url constructs a url from an id", {
   expect_identical(
     gutenberg_url(c(1, 23, 456), mirror = "base", verbose = FALSE),
