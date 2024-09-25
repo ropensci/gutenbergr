cd9ad5175c8e6e095af3ca5b1dd1739891a3040e
Jordan Bradford <36420801+jrdnbradford@users.noreply.github.com>
2024-09-25 14:41:59 -0400

    Activate `testthat` 3e (#80)
    * Use `testthat` 3e
    
    * Do not show col types

--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -60,8 +60,8 @@ test_that("All four datasets have a date-updated", {
   d3 <- attr(gutenberg_authors, "date_updated")
   d4 <- attr(gutenberg_languages, "date_updated")
 
-  expect_is(d1, "Date")
-  expect_is(d2, "Date")
-  expect_is(d3, "Date")
-  expect_is(d4, "Date")
+  expect_s3_class(d1, "Date")
+  expect_s3_class(d2, "Date")
+  expect_s3_class(d3, "Date")
+  expect_s3_class(d4, "Date")
 })
