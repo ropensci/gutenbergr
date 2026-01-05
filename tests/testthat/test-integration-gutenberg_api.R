cf4c961d61145fdfa2a24613ef35cecf03a50ed1
Jordan Bradford <36420801+jrdnbradford@users.noreply.github.com>
2026-01-05 15:40:06 -0500

    Use `helper` file

--- tests/testthat/test-integration-gutenberg_api.R
+++ tests/testthat/test-integration-gutenberg_api.R
@@ -1,12 +1,3 @@
-# Skip integration tests unless explicitly enabled
-skip_if_not_integration <- function() {
-  if (!identical(Sys.getenv("RUN_INTEGRATION_TESTS"), "true")) {
-    skip(
-      "Integration tests not enabled. Set RUN_INTEGRATION_TESTS=true to run."
-    )
-  }
-}
-
 test_that("gutenberg_download works with real API - single book", {
   skip_if_not_integration()
   skip_on_cran()
